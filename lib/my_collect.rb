446e0f1b19163d83f3938a3c101f70f2d4813f00
bblair31 <jbrennan.blair@gmail.com>
2018-09-10 13:02:54 -0400

    Done

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,9 @@
-
-
+def my_collect(array)
+  output_array = []
+  counter = 0
+  while counter < array.length
+    output_array << yield(array[counter].capitalize)
+    counter += 1
+  end
+  output_array
+end
