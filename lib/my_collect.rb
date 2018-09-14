083c1fb7bff930d15b46bc50e8dd315cf6063811
Casey Zimmer <czimmer@ramapo.edu>
2018-09-14 14:22:06 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,16 @@
 
-
+def my_collect(array)
+  
+  counter = 0
+  second_array = []
+  
+  while counter < array.size
+    
+    second_array.push(yield(array[counter]))
+    counter += 1
+    
+  end
+  
+  return second_array
+  
+end
